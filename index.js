bd6d7492e42193d706b0c8d584834820b6de0671
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-03-13 16:13:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1=2;
+const num2=31;
 
-// Write your code here
+const multiply=Number(num1)*Number(num2);
+console.log(multiply)
+
+
+const random = parseInt(Math.random(2, 1000)*100)
+console.log(random)
+const num3=28
+const num4=6
+const mod = num3 % num4
+
+
+
+ let max = Math.max(0,20)
+module.exports={
+    num1,num2,multiply,max,mod,random
+}
+console.log(max)
